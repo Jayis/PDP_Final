7ffe2b729a15bac509fc9308dbd9de5f4d288428
yftung7 <r02921044@ntu.edu.tw>
2014-06-14 14:08:29 +0800

    combine.c ( void combineWorld(...) )

--- combine.c
+++ combine.c
@@ -2,6 +2,7 @@
 #define NC 3
 
 // combine 4 images into 1, and update it into worldImage
+/*
 void combine(mat worldImage, vector<mat> mList, vector<camera> cList)
 {
     unsigned char * wArray = worldImage.data;
@@ -23,8 +24,41 @@ void combine(mat worldImage, vector<mat> mList, vector<camera> cList)
 	    int worldIndex = (x + y * worldImage.cols) * NC;
 	    int cameraIndex = ((x-left) + (y-down) * cList[index].width) * NC;
 	    for(int chnl = 0; chnl < NC; chnl++)
-		wArray[worldIndex + chnl] = mArray[cameraIndex + chnl];
+		wArray[worldIndex + chnl] = mArray[index][cameraIndex + chnl];
 	}
+    }
+}
+*/
+
+void combineWorld(mat worldImage, vector<mat> mList)
+{
+    unsigned char * wArray = worldImage.data;
+    vector<unsigned char * > mArray;
+    mArray.resize(mList.size());
+    for(int i = 0; i < mList.size(); i++)
+	mArray[i] = mList[i].data;
+
+    for(int x = 0; x < worldImage.cols; x++) for(int y = 0; y < worldImage.rows, y++)
+    {
+	int worldIndex = (x + y * worldImage.cols) * NC;
+	vector<unsigned int> sum;
+	sum.resize(NC);
+	for(int i = 0; i < NC; i++) sum[i] = 0;
+	int count = 0;
+
+	for(int ind = 0; ind < mList.size(); ind++)
+	{
+	    bool has_Value = false;
+	    for(int chnl = 0; chnl < NC; chnl++) if(mArray[ind][ worldIndex + chnl ] != 0)
+	    {
+		has_Value = true;
+		sum[chnl] += (unsigned int) mArray[ind][ worldIndex + chnl ];
+	    }
+	    if(has_Value) count++;
+	}
+
+	for(int chnl = 0; chnl < NC; chnl++)
+	    wArray[worldIndex + chnl] = sum[chnl] / count;
     }
 }
 
